190d7427380c163d6fa2853364c164807a7fbfc3
Fabio Henrique <59840567+fhenriquefcruz@users.noreply.github.com>
2026-06-02 09:17:56 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,27 @@
+const CACHE_NAME = 'versday-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/js/main.js',
+  '/js/api.js',
+  '/js/cache.js',
+  '/js/favorites.js',
+  '/js/history.js',
+  '/js/theme.js',
+  '/js/share.js',
+  '/js/ui.js',
+  '/js/background.js',
+  '/js/semantic.js',
+  '/manifest.json'
+];
 
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
