bc78979a704e11c6387d539268f9e2b3b744de38
Fabio Henrique <59840567+fhenriquefcruz@users.noreply.github.com>
2026-06-02 09:17:28 -0400

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,7 @@
+import { getHistory, addToHistory } from './history.js';
+import { getFavorites, addFavorite, removeFavorite, isFavorite } from './favorites.js';
+import { currentVerse, setCurrentVerse } from './main.js'; // circular? Melhor exportar do main
 
+export function updateFavoriteButton() { /* altera estilo do coração */ }
+export function renderFavoritesModal() { /* preenche a lista */ }
+export function renderHistoryModal() { /* preenche com últimos 50 */ }
