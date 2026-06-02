13392b7717a3f1862fac63ebcb2b94a2a7ac933f
Fabio Henrique <59840567+fhenriquefcruz@users.noreply.github.com>
2026-06-02 09:29:55 -0400

    Update semantic.js

--- js/semantic.js
+++ js/semantic.js
@@ -1,6 +1,9 @@
-// Mesma lógica de classificação semântica do código anterior, compactada
-const semanticThemes = [ /* ... manter os 13 temas com suas imagens ... */ ];
-const contemplativeImages = [ /* ... 10 imagens ... */ ];
+const semanticThemes = [
+  { name: 'alegria', keywords: ['alegria','alegre','júbilo','feliz','contentamento','regozijo','coração alegre'], images: ['https://images.pexels.com/photos/931018/pexels-photo-931018.jpeg','https://images.pexels.com/photos/1146762/pexels-photo-1146762.jpeg'] },
+  { name: 'paz', keywords: ['paz','calma','descanso','tranquilidade','serenidade'], images: ['https://images.pexels.com/photos/457881/pexels-photo-457881.jpeg'] },
+  // ... todos os outros temas (água, céu, montanha, etc.)
+];
+const contemplativeImages = ['https://images.pexels.com/photos/1191710/...', ...];
 
-export function classifyVerseTheme(verseText) { /* ... igual ao anterior */ }
-export function getImageForVerse(verseText) { /* ... igual ao anterior */ }
+export function classifyVerseTheme(verseText) { /* igual ao anterior */ }
+export function getImageForVerse(verseText) { /* igual ao anterior */ }
