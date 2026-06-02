aca9f2a8dde30bff402bacc52a50f633c401b525
Fabio Henrique <59840567+fhenriquefcruz@users.noreply.github.com>
2026-06-02 09:17:08 -0400

    Create semantic.js

---
+++ js/semantic.js
@@ -0,0 +1,6 @@
+// Mesma lógica de classificação semântica do código anterior, compactada
+const semanticThemes = [ /* ... manter os 13 temas com suas imagens ... */ ];
+const contemplativeImages = [ /* ... 10 imagens ... */ ];
+
+export function classifyVerseTheme(verseText) { /* ... igual ao anterior */ }
+export function getImageForVerse(verseText) { /* ... igual ao anterior */ }
